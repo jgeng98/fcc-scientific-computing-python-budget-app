1ebfbe8de5c77c16605e55f4c90e5c4c1d671e96
jgeng98 <77642029+jgeng98@users.noreply.github.com>
2021-05-25 20:51:45 -0400

    create outline of spend chart graph

--- budget.py
+++ budget.py
@@ -3,6 +3,7 @@ class Category:
         self.category = category
         self.balance = 0
         self.ledger = []
+        self.withdrawals = 0
 
     def __transaction_formatter(self, transaction):
         trimmed_description = transaction.get("description")[:23].ljust(23)
@@ -26,6 +27,7 @@ class Category:
         if self.check_funds(amount):
             self.ledger.append({"amount": -amount, "description": description})
             self.balance -= amount
+            self.withdrawals += amount
             return True
         else:
             return False
@@ -49,4 +51,31 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
+    width = 3 * len(categories) + 1
+    graph = "Percentage spent by category\n"
+    total = sum([category.withdrawals for category in categories])
+    percentages = [
+        round(category.withdrawals / total * 10) * 10 for category in categories
+    ]
+
+    for i in range(100, -1, -10):
+        graph += (str(i).rjust(3) + "| ").ljust(width)
+        graph += "\n"
+
+    graph += "    ".ljust(width + 4, "-")
+
+    return graph
+
+
+if __name__ == "__main__":
+    food = Category("food")
+    auto = Category("auto")
+    entertainment = Category("entertainment")
+    food.deposit(100)
+    auto.deposit(100)
+    entertainment.deposit(100)
+    food.withdraw(52)
+    auto.withdraw(28)
+    entertainment.withdraw(20)
+
+    print(create_spend_chart([food, auto, entertainment]))
